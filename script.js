843ee7e23ec9b523e03ec5b9761dd4fb68e389ed
bjack2197 <110922761+bjack2197@users.noreply.github.com>
2023-10-23 15:28:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+// script.js
+
+import products from './products.js';
+
+document.addEventListener("DOMContentLoaded", function () {
+    const productContainer = document.getElementById("product-list");
+    const searchInput = document.getElementById("search-input");
+    const searchButton = document.getElementById("search-button");
+
+    // Функция для отображения товаров
+    function displayProducts(productsToShow) {
+        productContainer.innerHTML = "";
+
+        productsToShow.forEach((product) => {
+            const productDiv = document.createElement("div");
+            productDiv.classList.add("product");
+
+            const productImage = document.createElement("img");
+            productImage.src = product.image;
+
+            const productName = document.createElement("h2");
+            productName.textContent = product.name;
+
+            const productPrice = document.createElement("p");
+            productPrice.textContent = `$${product.price.toFixed(2)}`;
+
+            productDiv.appendChild(productImage);
+            productDiv.appendChild(productName);
+            productDiv.appendChild(productPrice);
+            productContainer.appendChild(productDiv);
+        });
+    }
+
+    // Функция для обработки поискового запроса
+    function handleSearch() {
+        const searchQuery = searchInput.value.trim().toLowerCase();
+
+        const filteredProducts = products.filter((product) =>
+            product.name.toLowerCase().includes(searchQuery)
+        );
+
+        displayProducts(filteredProducts);
+    }
+
+    searchInput.addEventListener("input", handleSearch);
+    searchButton.addEventListener("click", handleSearch);
+
+    // Инициализация отображения всех товаров при загрузке страницы
+    displayProducts(products);
+});
